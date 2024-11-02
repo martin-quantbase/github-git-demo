5d439c7db47b4b200c69fdff25f7f4deae6b3dfd
wincanton <martinms@gmail.com>
2024-11-02 14:51:10 +0000

    Bobby commit changes to folder1/file1.py
    I have the suspiciaon that my colleague may change this file too ...

--- folder1/file1.py
+++ folder1/file1.py
@@ -2,3 +2,7 @@
 
 """
 # NOTE: Added this line - as a note to self.
+# I am bobby and I want to modify this.
+# I don't know if/when other colleagues of mine will be doing this.
+# But I will take the initiative.
+# Ok - this is it.
